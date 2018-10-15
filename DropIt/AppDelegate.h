4211db2d731225eaf2fd29ca95d02379156cd67a
Your Name <you@your-domain.com>
2018-10-15 16:08:40 +0900

    Initial Commit

---
+++ DropIt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DropIt
+//
+//  Created by JihoonPark on 15/10/2018.
+//  Copyright © 2018 JihoonPark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DropIt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DropIt
+//
+//  Created by JihoonPark on 15/10/2018.
+//  Copyright © 2018 JihoonPark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
